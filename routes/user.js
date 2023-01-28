6efa4a239e3e9458b789de34076360aa20f4d6f4
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-01-28 16:24:12 +0300

    build a function to retrieve the token from the “authorization” header by decoding it with JWT

--- routes/user.js
+++ routes/user.js
@@ -51,4 +51,24 @@ router.post("/login", (req, res) => {
     });
 });
 
+function authenticate(request, response, next) {
+  const authHeader = request.getHeader("Authorization");
+  const token = authHeader.split(" ")[1];
+  const secret = "SECRET";
+
+  jwt.verify(token, secret, (error, payload) => {
+    if (error) throw new Error("sign in error!");
+    database("users")
+      .where({ username: payload.username })
+      .first()
+      .then(() => {
+        request.user = user;
+        next();
+      })
+      .catch((error) => {
+        response.json({ message: error.message });
+      });
+  });
+}
+
 module.exports = router;
