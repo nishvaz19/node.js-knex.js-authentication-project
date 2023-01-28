b9490c00817963cbd7596ffabaa71e25da48e8de
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-01-28 15:41:29 +0300

    Set up the POST route for user login

--- routes/user.js
+++ routes/user.js
@@ -23,4 +23,32 @@ router.post("/users", (request, response) => {
   });
 });
 
+router.post("/login", (req, res) => {
+  const { user } = request.body;
+  database("users")
+    .where({ username: user.username })
+    .first()
+    .then((retrievedUser) => {
+      if (!retrievedUser) throw new Error("User Not Found");
+      return Promise.all([
+        bcrypt.compare(user.password, retrievedUser.password_hash),
+        Promise.resolve(retrievedUser),
+      ]).then((results) => {
+        const areSamePasswords = results[0];
+        if (!areSamePasswords) throw new Error("wrong password!");
+        const user = results[1];
+        const payload = { username: user.username };
+        const secret = "SECRET";
+        jwt
+          .sign(payload, secret, (error, token) => {
+            if (error) throw new Error(error);
+            response.json({ token, user });
+          })
+          .catch((error) => {
+            response.json({ message: error.message });
+          });
+      });
+    });
+});
+
 module.exports = router;
