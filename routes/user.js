d566349fef8e7aab1ce7e0f86f1d37656524a75c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-01-28 15:19:40 +0300

    set up the POST route for new users

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,23 @@ const database = require("../db/db.js");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+router.post("/users", (request, response) => {
+  const { user } = request.body;
+  bcrypt.hash(user.password, 12).then((hashed_password) => {
+    return database("users")
+      .insert({
+        username: user.username,
+        password_hash: hashed_password,
+      })
+      .returning("*")
+      .then((users) => {
+        const user = users[0];
+        response.json({ user });
+      })
+      .catch((err) => {
+        response.json({ error: err.message });
+      });
+  });
+});
+
 module.exports = router;
