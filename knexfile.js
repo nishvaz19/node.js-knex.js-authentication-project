c4e6d34db6d3acde5bb65d473560749a63bc97e5
nishvaz19 <44230467+nishvaz19@users.noreply.github.com>
2024-11-30 16:23:29 +0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,44 +5,54 @@
  */
 module.exports = {
   development: {
-    client: "mysql",
+    client: "postgresql",
+    searchPath: 'database_schema', //postgres: CREATE SCHEMA database_schema;create table database_schema.test (a int not null); OR ALTER ROLE aadvar12309_dropescape SET search_path TO database_schema;
     connection: {
-      user: "root",
-      host: "localhost",
-      password: "twekl2020",
-      database: "node-authentication-knex",
-      port: 3306,
+      user: "aadvar12309_dropescape",
+      host: "0iq5m.h.filess.io",
+      password: "846b24f0eec574cd33f16af209d271dcc12e7d08",
+      database: "aadvar12309_dropescape",
+      port: 5433,
     },
     migrations: {
       directory: __dirname + "/migrations",
+      tableName: 'knex_migrations'
     },
   },
 
   staging: {
-    client: "mysql",
+    client: "postgresql",
+    searchPath: 'database_schema', //postgres: CREATE SCHEMA database_schema;create table database_schema.test (a int not null); OR ALTER ROLE aadvar12309_dropescape SET search_path TO database_schema;
     connection: {
-      user: "root",
-      host: "localhost",
-      password: "twekl2020",
-      database: "node-authentication-knex",
-      port: 3306,
+      user: "aadvar12309_dropescape",
+      host: "0iq5m.h.filess.io",
+      password: "846b24f0eec574cd33f16af209d271dcc12e7d08",
+      database: "aadvar12309_dropescape",
+      port: 5433,
     },
     migrations: {
       directory: __dirname + "/migrations",
+      tableName: 'knex_migrations'
     },
   },
 
   production: {
-    client: "mysql",
+    client: "postgresql",
+    searchPath: 'database_schema', //postgres: CREATE SCHEMA database_schema;create table database_schema.test (a int not null); OR ALTER ROLE aadvar12309_dropescape SET search_path TO database_schema;
     connection: {
-      user: "root",
-      host: "localhost",
-      password: "twekl2020",
-      database: "node-authentication-knex",
-      port: 3306,
+      user: "aadvar12309_dropescape",
+      host: "0iq5m.h.filess.io",
+      password: "846b24f0eec574cd33f16af209d271dcc12e7d08",
+      database: "aadvar12309_dropescape",
+      port: 5433,
     },
     migrations: {
       directory: __dirname + "/migrations",
+      tableName: 'knex_migrations'
     },
   },
 };
+~                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                             
+"knexfile.js" 54 lines, 1832 bytes
